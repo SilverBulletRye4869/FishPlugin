307db1706d2c94b3169fc3571509e8087d0f599a
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-10-04 22:03:03 +0900

    計算方法作成

---
+++ src/main/java/silverassist/fishplugin/system/Calc.java
@@ -0,0 +1,102 @@
+package silverassist.fishplugin.system;
+
+import de.tr7zw.nbtapi.NBTItem;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Map;
+
+public class Calc {
+    private final double BASE_POWER = 10;
+    double CalcMain(PlayerFishEvent e){
+        Player p = e.getPlayer();
+        World w = p.getWorld();
+        double power = this.BASE_POWER;
+        power += CalcExp(p);
+        power += CalcLuck(p);
+        power += CalcRod(p);
+        power += CalcWhether(w);
+        power += CalcTime(w);
+        power += CalcMoon(w);
+        return power;
+    }
+    //ロッドの計算
+    private double CalcRod(Player p){
+        ItemStack item = p.getInventory().getItemInMainHand();
+        double RodRank = 0;
+        if(item.getType() == Material.AIR)return 0;
+        if(!new NBTItem(item).hasKey("fishpower"))return 0;
+        RodRank += new NBTItem(item).getDouble("fishpower");
+        RodRank += item.getEnchantmentLevel(Enchantment.LUCK) * 10.0; // + レベル*10
+        return RodRank;
+
+    }
+
+    //経験値レベルの計算
+    private double CalcExp(Player p){
+        double lv = p.getExpToLevel();
+        double power = 4.0 * Math.sqrt(lv);
+        return Math.min(40,power);
+    }
+
+    //幸運の計算
+    private double CalcLuck(Player p){
+        double luck = p.getAttribute(Attribute.GENERIC_LUCK).getValue();
+        double maxluck = 5;
+        double power = Math.max( (30.0 / maxluck) * Math.min(maxluck, luck) , -30.0);
+        return power;
+    }
+
+    //宝釣りの計算
+    private double CalcEnch(ItemStack item){
+
+        return 0;
+    }
+
+    //天気の計算
+    private double CalcWhether(World w){
+        double power = 0;
+        if(w.hasStorm())power +=20;
+        if(w.isThundering())power +=10;
+        return power;
+    }
+
+
+    //時間の計算
+    private double CalcTime(World w){
+        double time = (w.getTime()%24000)/1000 + 6.0;
+        if(time>=24) time-=24;
+
+        if((time>=4.5&&time<6.0) || (time>=18&&time<=19.5))return 30; //4:30-6:00, 18:00-19:30
+        else if((time>=9&&time<=15)||time>=21.3||time<=2.7)return -20;//9:00-15:00, 21:18-2:42
+        return 0; //その他
+    }
+
+    private double CalcMoon(World w){
+        int moonType = (int) (Math.floor(w.getFullTime() / 24000) % 8);
+        switch (moonType){
+            //満月
+            case 0:
+                return 30;
+            //十三夜,十八夜
+            case 1:
+            case 7:
+                return 10;
+            //三日月, 二十五夜
+            case 3:
+            case 5:
+                return -5;
+            //新月
+            case 4:
+                return -20;
+            //その他
+            default:
+                return 0;
+        }
+    }
+}
