2d4888bb7663c3cddb4eac767eb5c03d53c7abf9
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-10-06 10:01:57 +0900

    コマンド登録

--- src/main/java/silverassist/fishplugin/FishPlugin.java
+++ src/main/java/silverassist/fishplugin/FishPlugin.java
@@ -1,19 +1,33 @@
 package silverassist.fishplugin;
 
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import silverassist.fishplugin.command.Rod;
 import silverassist.fishplugin.system.MainSystem;
 
+import java.util.Map;
+
 public final class FishPlugin extends JavaPlugin {
+
     public static JavaPlugin plugin = null;
+    public static Map<String, Map<String, Map<String, String>>> fishData;
+
     @Override
     public void onEnable() {
         plugin = this;
         // Plugin startup logic
-        getServer().getPluginManager().registerEvents(new MainSystem(),this);
+        getServer().getPluginManager().registerEvents(new MainSystem(), this);
+        PluginCommand command = getCommand("fishpower");
+        if (command != null) command.setExecutor(new Rod());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void FishRegister() {
+
     }
 }
