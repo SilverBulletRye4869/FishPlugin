954de2b9a197283e06054a53474407b91a8bedb4
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-10-06 10:02:31 +0900

    ハズレアイテム設定

--- src/main/java/silverassist/fishplugin/system/MainSystem.java
+++ src/main/java/silverassist/fishplugin/system/MainSystem.java
@@ -1,25 +1,38 @@
 package silverassist.fishplugin.system;
 
 import org.bukkit.Material;
-import org.bukkit.WeatherType;
-import org.bukkit.World;
 import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.inventory.ItemStack;
-import silverassist.fishplugin.system.Calc;
+import org.bukkit.inventory.meta.ItemMeta;
+import silverassist.fishplugin.system.calc.Calc;
 
-public class MainSystem implements Listener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainSystem implements Listener{
     @EventHandler
     public void onPlayerFish(PlayerFishEvent e) {
-        ItemStack item = new ItemStack(Material.LAVA_BUCKET);
+        ItemStack item = null;
         Item FishItem = (Item) e.getCaught();
         if(FishItem==null)return;
 
         double power = new Calc().CalcMain(e);
 
+        //釣りパワーが負の時はハズレアイテムにする
+        if(power < 0){
+            item = new ItemStack(Material.KELP);
+            ItemMeta meta = item.getItemMeta();
+            meta.setDisplayName("§a§lコンブ");
+            List<String> lore = new ArrayList<>();
+            lore.set(0,"§f§l釣りパワーが足りないようだ");
+            meta.setLore(lore);
+            item.setItemMeta(meta);
+            FishItem.setItemStack(item);
+            return;
+        }
 
     }
 
