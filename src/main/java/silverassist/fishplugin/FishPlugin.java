750717ba051170d7a20825f61c36c8f83cd434de
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-10-06 22:20:43 +0900

    Config制作設定

--- src/main/java/silverassist/fishplugin/FishPlugin.java
+++ src/main/java/silverassist/fishplugin/FishPlugin.java
@@ -1,22 +1,30 @@
 package silverassist.fishplugin;
 
+import org.bukkit.block.Biome;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.fishplugin.command.Rod;
 import silverassist.fishplugin.system.MainSystem;
 
+import java.util.List;
 import java.util.Map;
 
 public final class FishPlugin extends JavaPlugin {
 
     public static JavaPlugin plugin = null;
-    public static Map<String, Map<String, Map<String, String>>> fishData;
+    public static Map<String, Map<String,List<Map<?, ?>> > > fishData;
 
     @Override
     public void onEnable() {
+        this.saveDefaultConfig();
         plugin = this;
         // Plugin startup logic
+        for(Biome biome: Biome.values()){
+            if(getConfig().get(biome.name()) !=null)continue;
+            getConfig().set(biome.name(), "");
+        }
+        saveConfig();
+
         getServer().getPluginManager().registerEvents(new MainSystem(), this);
         PluginCommand command = getCommand("fishpower");
         if (command != null) command.setExecutor(new Rod());
@@ -27,7 +35,5 @@ public final class FishPlugin extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    private void FishRegister() {
 
-    }
 }
