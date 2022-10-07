cd8a4dc0f35986f7feaf5aee3fdddeed178db4e1
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-10-07 21:52:23 +0900

    魚抽選実装完了

--- src/main/java/silverassist/fishplugin/system/MainSystem.java
+++ src/main/java/silverassist/fishplugin/system/MainSystem.java
@@ -5,6 +5,7 @@ import org.bukkit.block.Biome;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
@@ -14,45 +15,36 @@ import silverassist.fishplugin.FishPlugin;
 import silverassist.fishplugin.system.calc.Calc;
 
 import java.util.*;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainSystem implements Listener{
     @EventHandler
     public void onPlayerFish(PlayerFishEvent e) {
 
-        ItemStack item = null;
+        ItemStack item;
         Item FishItem = (Item) e.getCaught();
         if(FishItem==null)return;
 
         double power = new Calc().CalcMain(e);
-
         //釣りパワーが負の時はハズレアイテムにする
         if(power < 0){
-            List<String> lore = new ArrayList<>();
-            lore.set(0,"§f釣りパワーが不足しているようだ");
-            item = CreateItem(Material.KELP, "§a§l水草", lore,0);
-            FishItem.setItemStack(item);
+            FishItem.setItemStack(HazleItem());
             return;
         }
-        item = DecideItem(power, e.getPlayer().getWorld().getBiome(e.getPlayer().getLocation()));
+        item = DecideItem(power, e.getPlayer());
 
+        FishItem.setItemStack(item);
     }
 
-    private ItemStack CreateItem(Material material, String name, List<String> lore, int model){
-        ItemStack item = new ItemStack(material);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(name);
-        meta.setLore(lore);
-        item.setItemMeta(meta);
-        return item;
-    }
-
-    private ItemStack DecideItem(double power, Biome biome){
+    private ItemStack DecideItem(double power, Player p){
         FileConfiguration config = FishPlugin.plugin.getConfig();
-        Map<String,Integer> fishData = new HashMap<>();
+        Map<String,Integer> fishData = new LinkedHashMap<>();
         AtomicInteger totalWeight = new AtomicInteger(0);
+        Biome biome = p.getWorld().getBiome(p.getLocation());
 
         String[] paths = {"ANY",biome.name()};
+        int border = 0; // ANYとbiomeの境
         for(String path: paths) {
             ConfigurationSection keyP = config.getConfigurationSection(path);
             if (keyP != null) {
@@ -60,16 +52,68 @@ public class MainSystem implements Listener{
                     int min_weight = config.getInt(path+"." + key + ".min_weight");
                     if (min_weight > power) return;
                     int weight = config.getInt(path+"." + key + ".weight");
-                    if (Objects.isNull(weight) || weight == 0) return;
-                    fishData.put(key, totalWeight.get());
+                    if (weight == 0) return;
+                    String perm = config.getString(path+"."+key+".permission");
+                    if(perm!=null){
+                        if(!p.hasPermission(perm))return;
+                    }
                     totalWeight.addAndGet(weight);
+                    fishData.put(key, totalWeight.get());
                 });
             }
+            border = totalWeight.get();
         }
-        ItemStack item = null;
-        return item;
+        if(totalWeight.get()==0)return HazleItem(); //魚が見つからなければハズレを返す
+
+
+        //魚決定
+        String fish = null;
+        int num = (int)Math.floor(Math.random() * totalWeight.get());
+        for(String fishType: fishData.keySet()){
+            FishPlugin.plugin.getServer().broadcastMessage(fishData.get(fishType) +"<-"+num);
+            if(fishData.get(fishType)>num){
+                fish = fishType;
+                break;
+            }
+        }
+
+        //魚のキー
+        String fishKey;
+        if(num < border)fishKey= "ANY."+fish;
+        else fishKey = biome.name() +"."+fish;
+
+        //item-type
+        String fishTypeStr = config.getString(fishKey + ".item-material");
+        if(fishTypeStr==null)return ErrorPaper(power,biome,"「"+fishKey+"」にitem-materialがセットされていません");
+        Material fishType = Material.getMaterial(fishTypeStr);
+        if(fishType == null)return ErrorPaper(power,biome,"アイテム「"+fishTypeStr+"」は存在しません. ("+fishKey+")");
+
+        return CreateItem(Material.getMaterial(fishTypeStr),config.getString(fishKey+".name"), (List<String>) config.get(fishKey + ".lore"),config.getInt(fishKey));
     }
 
+    private ItemStack CreateItem(Material material, String name, List<String> lore, int model){
+        ItemStack item = new ItemStack(material);
 
+        ItemMeta meta = item.getItemMeta();
+        if(name!=null)meta.setDisplayName(name);
+        if(lore!=null)meta.setLore(lore);
+        meta.setCustomModelData(model);
+        item.setItemMeta(meta);
+        return item;
+    }
+    private ItemStack ErrorPaper(double power, Biome biome, String reason){
+        List<String> lore = new ArrayList<>();
+        lore.add("§f§lエラーが発生しました。");
+        lore.add("§f§lこの紙を運営に渡してください。");
+        lore.add("§6内容: "+ reason);
+        lore.add("§6釣りパワー: "+power + "§6 ,バイオーム: "+biome.name());
+        return CreateItem(Material.PAPER, "§c§lエラーペーパー", lore, 0);
+    }
+
+    private ItemStack HazleItem(){
+        List<String> lore = new ArrayList<>();
+        lore.add("§f釣りパワーが不足しているようだ");
+        return CreateItem(Material.KELP, "§a§l水草", lore,0);
+    }
 
 }
