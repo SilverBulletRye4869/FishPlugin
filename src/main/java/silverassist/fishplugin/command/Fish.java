858054c739ae020141806d270c0bbace19f17470
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-10-08 20:40:33 +0900

    釣りモードコマンド追加

--- src/main/java/silverassist/fishplugin/command/Fish.java
+++ src/main/java/silverassist/fishplugin/command/Fish.java
@@ -3,23 +3,38 @@ package silverassist.fishplugin.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import silverassist.fishplugin.FishPlugin;
+import silverassist.fishplugin.system.MainSystem;
+
+import java.util.List;
 
 public class Fish implements CommandExecutor {
+    private final String prefix = "§b§l[§e§lFishPlugin§b§l]§r";
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player p = (Player)sender;
+        if(p==null)return true;
         if(args.length<1){
-            /*helpの処理*/
+            sender.sendMessage(prefix+"§a§l現在の釣りモードは§d§l"+MainSystem.fishModeTrue.contains(p)+"§a§lです");
             return true;
         }
 
         switch (args[0]){
-            case "reload":
-                new FishPlugin().saveDefaultConfig();
-                sender.sendMessage("§a§lconfigをreloadしました");
+            case "modechange":
+                if(MainSystem.fishModeTrue.contains(p)){
+                    MainSystem.fishModeTrue.remove(p);
+                    p.sendMessage(prefix+"§a§l釣りモードを§d§lfalse§a§lにしました");
+                }
+                else {
+                    MainSystem.fishModeTrue.add(p);
+                    p.sendMessage(prefix+"§a§l釣りモードを§d§ltrue§a§lにしました");
+                }
                 break;
+
+
         }
-        return false;
+        return true;
     }
 
 }
